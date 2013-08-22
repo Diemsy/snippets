cdcaa283c387f0e422c8db35a1cc138ef8deb965
Kris Olszewski <KrisOlszewski@users.noreply.github.com>
2013-08-22 17:16:02 -0400

    Update placeholder-polyfill.js

--- js/placeholder-polyfill.js
+++ js/placeholder-polyfill.js
@@ -1,7 +1,7 @@
 /**
   * Placeholder polyfill
   */
-(function($) {
+$(function() {
 
   var placeholder = $('[placeholder]');
   
@@ -30,4 +30,10 @@
   
   });
   
-})(jQuery);
+});
+
+
+/**
+  * Placeholder polyfill minified
+  */
+$(function(){var placeholder = $('[placeholder]');placeholder.on('focus', function() {var input = $(this);if (input.val() === input.attr('placeholder')) {input.val('');input.removeClass('placeholder');}}).on('blur', function() {var input = $(this);if (input.val() === '' || input.val() === input.attr('placeholder')) {input.addClass('placeholder');input.val(input.attr('placeholder'));}}).blur();placeholder.parents('form').on('submit', function() {$(this).find('[placeholder]').each(function() {var input = $(this);if (input.val() === input.attr('placeholder')) {input.val('');}});});});
