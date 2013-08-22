e468acc31004f54f71102ae8a2e851acffaa321d
Kris Olszewski <KrisOlszewski@users.noreply.github.com>
2013-08-21 22:00:46 -0400

    Update post-message.js

--- js/post-message.js
+++ js/post-message.js
@@ -14,23 +14,23 @@
   
 window.onload = function() {
   var domain = "http://www.destination-domain.com";
-  var iframe = document.getElementById('post-message-iframe').contentWindow;
+  var iframe = document.getElementById("post-message-iframe").contentWindow;
   
-  var message = '';
+  var message = "";
   
   iframe.postMessage(message, domain);
   
-  window.addEventListener('message', function(event) {
+  window.addEventListener("message", function(event) {
     if (event.origin !== domain) return;
-    console.log('response: ', event.data);
+    console.log("response: ", event.data);
   }, false);
 };
 
 /**
   * Receiver
   */
-window.addEventListener('message', function(event) {
+window.addEventListener("message", function(event) {
   if (event.origin !== "http://www.sender-domain.com") return;
-  var data = (localStorage.id !== undefined) ? localStorage.id : '';
+  var data = (localStorage.id !== undefined) ? localStorage.id : "";
   event.source.postMessage(data, event.origin);
 }, false);
