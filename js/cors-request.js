ee824f673aa329e601a4d874393a1020ee731530
Kris Olszewski <kris.olszewski@gmail.com>
2013-08-24 11:38:26 -0400

    new snippet

---
+++ js/cors-request.js
@@ -0,0 +1,60 @@
+/**
+  * Create CORS object
+  */
+function createCORSrequest(method, url) {
+  
+  // Create XHR object
+  var xhr = new XMLHttpRequest();
+  
+  if ("withCredentials" in xhr) {
+  
+    // XMLHTTPRequest2 object
+    xhr.open(method, url, true);
+    
+  } else if (typeof XDomainRequest != "undefined") {
+  
+    // XDomainRequest object for IE
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+    
+  } else {
+  
+    // CORS not supported by browser
+    xhr = null;
+    
+  }
+  
+  return xhr;
+}
+
+/**
+  * Make CORS request
+  */
+function makeCORSrequest() {
+  
+  // Set URL to CORS enabled server
+  // ColdFusion example: <cfheader name="Access-Control-Allow-Origin" value="*">
+  var url = 'http://www.domain.com';
+  
+  // Create CORS request
+  var xhr = createCORSRequest('GET', url);
+  
+  // CORS not supported 
+  if (!xhr) {
+    console.log('Not supported');
+    return;
+  }
+  
+  // On successful request
+  xhr.onload = function() {
+    console.log(xhr.responseText);
+  };
+  
+  // On error
+  xhr.onerror = function() {
+    console.log('Error making the request.');
+  };
+  
+  // Send request
+  xhr.send();
+}
