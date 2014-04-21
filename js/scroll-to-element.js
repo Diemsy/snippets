0754520aa0911ca78f6d22b5d9557a01bf1b31ad
Kris Olszewski <KrisOlszewski@users.noreply.github.com>
2014-04-21 14:08:59 -0400

    Update scroll-to-element.js

--- js/scroll-to-element.js
+++ js/scroll-to-element.js
@@ -2,28 +2,26 @@
   * Scroll to element
   * --------------------------------------------------
   *
-  * Recommended usage
-  * <a data-scroll-trigger="area">Trigger</a>
-  * <div data-scroll-target="area">Target</div>
+  * Recommended usage:
+  * <span data-scroll-trigger="{id}">Trigger element</span>
+  * <div data-scroll-target="{id}">Target element</div>
   *
   */
 
 ;(function($) {
 
   'use strict';
-  
-  var container = $('html, body');
-  var trigger = $('[data-scroll-trigger]');
-  
-  trigger.on('click', function(e) {
+
+  $(document).on('click.scrollToElement', '[data-scroll-trigger]', function(e) {
+
     e.preventDefault();
-    
+
     var id = $(this).data('scroll-trigger');
-    
-    container.animate({
+
+    $('html, body').animate({
       scrollTop: $('[data-scroll-target="' + id + '"]').offset().top
     }, 500);
-    
+
   });
-  
+
 })(jQuery);
