700a3e297ac3e3664d6996085dd5b667d6e8414d
Kris Olszewski <KrisOlszewski@users.noreply.github.com>
2014-04-21 15:23:58 -0400

    Update module-pattern.js

--- js/module-pattern.js
+++ js/module-pattern.js
@@ -11,21 +11,35 @@ var Module = (function (api, window, document, undefined) {
   };
   
   api.increment = function (value) {
-    return ( counter += value || 1 );
+    counter += value || 1;
+    return counter;
   };
   
   api.decrement = function (value) {
-    return ( counter -= value || 1 );
+    counter -= value || 1;
+    if (counter < 0) {
+      counter = 0;
+    }
+    return counter;
   };
   
   api.set = function (value) {
-    return ( counter = value );
+    counter = value;
+    return counter;
   };
   
   api.reset = function () {
-    return ( counter = 0 );
+    counter = 0;
+    return counter;
   };
   
   return api;
   
 })(Module || {}, window, document);
+
+console.log(Module.get()); // 0
+console.log(Module.increment()); // 1
+console.log(Module.increment(10)); // 11
+console.log(Module.decrement(5)); // 6
+console.log(Module.decrement()); // 5
+console.log(Module.reset()); // 0
